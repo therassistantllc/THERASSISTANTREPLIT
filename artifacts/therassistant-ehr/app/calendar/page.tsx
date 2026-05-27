1a7e2a23bbd43c51c4ba633b9bc2adaa0cb93d69
therassistantllc <admin@therassistant.com>
2026-05-27 02:24:38 +0000

    refactor: Use dynamic import for MonthCalendarClient to disable server-side rendering

--- artifacts/therassistant-ehr/app/calendar/page.tsx
+++ artifacts/therassistant-ehr/app/calendar/page.tsx
@@ -1,4 +1,8 @@
-import MonthCalendarClient from "./MonthCalendarClient";
+import dynamic from "next/dynamic";
+
+const MonthCalendarClient = dynamic(() => import("./MonthCalendarClient"), {
+  ssr: false,
+});
 
 export default function CalendarPage() {
   return <MonthCalendarClient />;
